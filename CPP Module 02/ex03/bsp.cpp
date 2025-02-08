3a1dc05776b3429b9a605c0ad591df69775ce1ae
ABDE-LKADER <abadouab@student.1337.ma>
2025-02-08 09:38:54 +0100

    CHECK POINT <MODULE 02 FINISHED> ...

--- CPP Module 02/ex03/bsp.cpp
+++ CPP Module 02/ex03/bsp.cpp
@@ -1,5 +1,6 @@
 # include "Point.hpp"
 
 bool bsp( Point const a, Point const b, Point const c, Point const point ) {
-	return (a.area(b, c) == (point.area(a, b) + point.area(b, c) + point.area(c, a)));
+	return (point.area(a, b).toFloat() && point.area(b, c).toFloat() && point.area(c, a).toFloat()
+			&& a.area(b, c) == point.area(a, b) + point.area(b, c) + point.area(c, a));
 }
--- CPP Module 02/ex03/main.cpp
+++ CPP Module 02/ex03/main.cpp
@@ -3,13 +3,13 @@
 int	main(void)
 {
 	Point	a(0, 0);
-	Point	b(0, 0);
-	Point	c(0, 0);
-	Point	point(0, 0);
+	Point	b(10, 10);
+	Point	c(0, 20);
+	Point	point(1, 15);
 
 	if (bsp(a, b, c, point) == true)
 		std::cout << "The point is inside the triangle" << std::endl;
 	else
-		std::cout << "The point outside the triangle" << std::endl;
+		std::cout << "The point is outside the triangle" << std::endl;
 	return 0;
 }
